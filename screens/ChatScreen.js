1b764f1027b80427f0f458db5c72b7b6a6f29925
Corey Vernon <coreyjvernon@gmail.com>
2019-05-22 17:57:59 -0400

    add message option

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import styles from '../constants/styles';
 
 export default class ChatScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('name', null)
+    }
+  }
+
+  state = {
+    textMessage: ''
+  }
+
+  handleChange = key => val => {
+    this.setState({ [key]:val })
+  }
+
+  sendMessage = async () => {
+
+  }
+
   render(){
     return (
-      <View>
-        <Text>
-          Chat Screen
-        </Text>
-      </View>
+      <SafeAreaView>
+        <View style={{flexDirection:'row', alignItems:'center'}} >
+          <TextInput 
+            style = {styles.input}
+            value = {this.state.textMessage}
+            placeholder = 'type message...'
+            onChangeText={this.handleChange('textMessage')}
+          />
+          <TouchableOpacity onPress={this.sendMessage}>
+            <Text style={styles.btnText}>
+              Send
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+      </SafeAreaView>
     )
   }
 }
